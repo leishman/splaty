cb0ad8f4a868f3cda44bb8fd7c0612aaa79251b6
Alexander Leishman <leishman3@gmail.com>
2014-09-12 22:59:03 +0800

    begin adding functionality to command executor

--- app/services/command_executor.rb
+++ app/services/command_executor.rb
@@ -1,7 +1,11 @@
 class CommandExecutor
-	def initialize(attrs)
-		@wall = attrs[:wall]
-		@command = attrs[:command]
-		raise ArgumentError.new 'Must have wall and command' unless @wall && @command
-	end
+  def initialize(attrs)
+    @wall = attrs[:wall]
+    @command = attrs[:command]
+    raise ArgumentError.new 'Must have wall and command' unless @wall && @command
+  end
+
+  def run!
+    SplatyMailer.wall_copy(@wall, 'leishman3@gmail.com')
+  end
 end
