c67b42e8aaff5b92911da9943b1bd2646ee9d59e
Alexander Leishman <leishman3@gmail.com>
2014-09-12 23:00:33 +0800

    update test.rb host

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'www.example.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
