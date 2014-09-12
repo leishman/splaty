0c2697a000c576892deb59435295630030f2818e
Alexander Leishman <leishman3@gmail.com>
2014-09-12 22:18:00 +0800

    add mail settings to config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
